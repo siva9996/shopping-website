c4737a2d2a393eb0ad513a638ac82ea45a0596ae
siva9996 <rmsiva9996@gmail.com>
2023-10-28 13:20:56 +0530

    add shopping added requirements website

--- shoping/settings.py
+++ shoping/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +121,9 @@ STATIC_URL = 'static/'
 
 MEDIA_URL='uploads/'
 
-STATICFILES_DIRS=[ BASE_DIR / 'static' ]
+STATICFILES_DIRS=[
+    BASE_DIR / 'staic'
+]
 
 MEDIA_ROOT=BASE_DIR / 'static'
 

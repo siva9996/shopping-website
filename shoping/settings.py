0f20d97cda1ac3fdf24f90c6eebbaee8238ac3f9
siva9996 <rmsiva9996@gmail.com>
2023-10-28 12:26:03 +0530

    add shopping added requirement website

--- shoping/settings.py
+++ shoping/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-wf)2#0$37aletl&fe&j98)dcoaucyln7#5w&aj$co*t_%@!*tk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
